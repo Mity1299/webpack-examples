6360ecdea10264a5d6534e93bffe43af22c3903a
meqi.wu <meiqi.wu@dorabot.com>
2023-03-07 10:12:33 +0800

    feat: 整理webpack配置

--- common-chunk-and-vendor-chunk/webpack.config.js
+++ common-chunk-and-vendor-chunk/webpack.config.js
@@ -1,37 +1,51 @@
 var path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const DashboardPlugin = require("webpack-dashboard/plugin");
 
 module.exports = {
-	mode: "development",
-	// mode: "production",
-	entry: {
-		pageA: "./pageA",
-		pageB: "./pageB",
-		pageC: "./pageC"
-	},
-	optimization: {
-		chunkIds: "named",// 指定打包过程中的chunkId，设为named会生成可读性好的chunkId，便于debug
-		splitChunks: {
-			minSize: 0,// 默认30000（30kb），但是demo中的文件都很小，minSize设为0，让每个文件都满足大小条件
-			// name:false,
-			cacheGroups: {
-				commons: {
-					chunks: "all",//加入按需加载后，设为all将所有模块包括在优化范围内
-					// name: "commons",
-					minChunks: 2,
-					maxInitialRequests: 5, // 默认为3，无法满足我们的分包数量
-				},
-				vendor: {
-					test: /node_modules/,
-					chunks: "initial",
-					name: "vendor",
-					// priority: 10,
-					// enforce: true
-				}
-			}
-		}
-	},
-	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "[name].js",
-	}
+  plugins: [
+    /**
+     * All files inside webpack's output.path directory will be removed once, but the
+     * directory itself will not be. If using webpack 4+'s default configuration,
+     * everything under <PROJECT_DIR>/dist/ will be removed.
+     * Use cleanOnceBeforeBuildPatterns to override this behavior.
+     *
+     * During rebuilds, all webpack assets that are not used anymore
+     * will be removed automatically.
+     *
+     * See `Options and Defaults` for information
+     */
+    new CleanWebpackPlugin(),
+    new DashboardPlugin(),
+  ],
+
+  mode: "development",
+  // mode: "production",
+  entry: {
+    pageA: "./src/pageA",
+    pageB: "./src/pageB",
+    pageC: "./src/pageC",
+  },
+  optimization: {
+    chunkIds: "named", // 指定打包过程中的chunkId，设为named会生成可读性好的chunkId，便于debug
+    splitChunks: {
+      minSize: 0, // 默认30000（30kb），但是demo中的文件都很小，minSize设为0，让每个文件都满足大小条件
+      cacheGroups: {
+        commons: {
+          chunks: "initial",
+          minChunks: 2,
+          maxInitialRequests: 5, // 默认为3
+        },
+        vendor: {
+          test: /node_modules/,
+          chunks: "initial",
+          name: false,
+        },
+      },
+    },
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
 };
