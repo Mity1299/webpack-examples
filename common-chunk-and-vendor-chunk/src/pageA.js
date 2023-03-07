15a2a825be6771a5960ea2fe97133c8504af91e2
meqi.wu <meiqi.wu@dorabot.com>
2023-03-07 15:46:09 +0800

    feat: 按需加载测试2

--- common-chunk-and-vendor-chunk/src/pageA.js
+++ common-chunk-and-vendor-chunk/src/pageA.js
@@ -4,8 +4,8 @@ import utility2 from "./utility2";
 
 export default () => {
   //懒加载
-  import("./async1");
-  import("./async2");
+  // import("./async1");
+  // import("./async2");
 
   console.log("pageA");
 };
--- common-chunk-and-vendor-chunk/src/pageB.js
+++ common-chunk-and-vendor-chunk/src/pageB.js
@@ -3,5 +3,7 @@ import utility2 from "./utility2";
 import utility3 from "./utility3";
 
 export default () => {
+  import("./async1");
+  import("./async2");
   console.log("pageB");
 };
