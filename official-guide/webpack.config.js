482ee4c49c41af4a9d5e5ff5b66491cec84bbf26
mity <807798223@qq.com>
2023-03-11 15:48:10 +0800

    step: 加载CSS文件

--- official-guide/webpack.config.js
+++ official-guide/webpack.config.js
@@ -3,7 +3,21 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    //从 JavaScript 模块中导入 CSS 文件
+    //即import './style.css'
+    rules: [
+      {
+        test: /\.css$/i,
+        /**
+         * 先执行style-loader，然后执行css-loader
+         * style-loader会使打包后的文件，是用js动态创建style标签进行引入
+         */
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
