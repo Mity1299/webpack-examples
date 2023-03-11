9816a4a35e47b4485758fb0f52309ba462062daa
mity <807798223@qq.com>
2023-03-11 15:53:45 +0800

    step: 加载图片

--- official-guide/webpack.config.js
+++ official-guide/webpack.config.js
@@ -18,6 +18,15 @@ module.exports = {
          */
         use: ["style-loader", "css-loader"],
       },
+      /**
+       * 加载图像
+       * 允许JS引入图像，最终会将MyImage转化成打包后的image的url
+       * import MyImage from './my-image.png'
+       */
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
